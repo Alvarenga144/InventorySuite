f12c02a64d15578677c0f86fcedf05c010794911
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2025-11-09 13:07:52 -0600

    refact: change ui styles and integrate icons and swals

--- Inventory.Api/Inventory.Web/Controllers/HomeController.cs
+++ Inventory.Api/Inventory.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Inventory.Web.Filters;
 using Inventory.Web.Helpers;
 using Inventory.Web.Models;
+using Inventory.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -11,18 +12,47 @@ namespace Inventory.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IApiService _apiService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IApiService apiService)
         {
             _logger = logger;
+            _apiService = apiService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             // Obtener datos del usuario desde la sesión
             var userData = SessionHelper.GetUserData<UserSessionData>(HttpContext.Session);
             ViewBag.UserName = userData != null ? $"{userData.Nombre} {userData.Apellido}" : "Usuario";
-            
+
+            var token = SessionHelper.GetToken(HttpContext.Session);
+
+            try
+            {
+                var productosResponse = await _apiService.GetProductosAsync(token);
+                ViewBag.TotalProductos = productosResponse.Success && productosResponse.Data != null
+                    ? productosResponse.Data.Count
+                    : 0;
+            }
+            catch
+            {
+                ViewBag.TotalProductos = 0;
+            }
+
+            try
+            {
+                var ventasResponse = await _apiService.GetVentasAsync(token);
+                var hoy = DateTime.Today;
+                ViewBag.TotalVentasHoy = ventasResponse.Success && ventasResponse.Data != null
+                    ? ventasResponse.Data.Count(v => v.Fecha.Date == hoy)
+                    : 0;
+            }
+            catch
+            {
+                ViewBag.TotalVentasHoy = 0;
+            }
+
             return View();
         }
 
